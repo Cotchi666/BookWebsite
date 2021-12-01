98dee7067ef74472063caa520cded76258ad58eb
Cotchi666 <chiennguyen.170901@gmail.com>
2021-12-01 19:18:52 +0700

    first commit

---
+++ MyProject_01/MyProject_01/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyProject_01.Context;
+namespace MyProject_01.Controllers
+{
+    public class ProductController : Controller
+    {
+        WebBanSachEntities db = new WebBanSachEntities();
+        // GET: Product
+        public ActionResult Detail(int Id)
+        {
+            var objProduct = db.Products.Where(n => n.Id == Id).FirstOrDefault();//lấy 1 cái duy nhất;
+            return View(objProduct);
+        }
+
+    }
+}
